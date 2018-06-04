90344a83fa03a8acad113ee176c29a8b42901a8e
bunkdeath <kshetriamrit@gmail.com>
2018-06-04 21:11:49 +0545

    new theme for website

--- static/js/web-tracker.js
+++
@@ -1,7 +0,0 @@
-document.writeln('<script async src="https://www.googletagmanager.com/gtag/js?id=UA-47136525-7"></script>');
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'UA-47136525-7');
